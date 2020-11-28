b80b81c956fe61d4c90013cf6e30aed0f6b9af56
Davide Oggioni (Reti) <daviogg@hotmail.it>
2020-11-28 11:00:05 +0100

    Add validators and regex

--- src/app/components/subscribe/subscribe.component.ts
+++ src/app/components/subscribe/subscribe.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SubscribeComponent implements OnInit {
 
   subscribeForm: FormGroup;
+  private phoneNumberRegExp = new RegExp('^(\\((00|\\+)39\\)|(00|\\+)39)?(38[890]|34[7-90]|36[680]|33[3-90]|32[89])\\d{7}');
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -16,7 +17,7 @@ export class SubscribeComponent implements OnInit {
     this.subscribeForm = this.formBuilder.group({
       firstName: [null, Validators.required],
       lastName: [null, Validators.required],
-      phoneNumber: [null, Validators.pattern('[0-9 ]{11}')],
+      phoneNumber: [null, Validators.pattern(this.phoneNumberRegExp)],
       email: [null, [Validators.required, Validators.email]]
     });
   }
