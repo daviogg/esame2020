ab6eaf1e36592077a3177519dc4fb0d1c6b6be29
Davide Oggioni (Reti) <daviogg@hotmail.it>
2020-11-28 11:31:24 +0100

    Add subscribe

--- src/app/components/subscribe/subscribe.component.ts
+++ src/app/components/subscribe/subscribe.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { SignedUp } from 'src/app/models/signedUp';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
   selector: 'app-subscribe',
@@ -9,9 +12,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SubscribeComponent implements OnInit {
 
   subscribeForm: FormGroup;
+  private signedUp: SignedUp;
+  private trainId: string;
   private phoneNumberRegExp = new RegExp('^(\\((00|\\+)39\\)|(00|\\+)39)?(38[890]|34[7-90]|36[680]|33[3-90]|32[89])\\d{7}');
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private backendService: BackendService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.subscribeForm = this.formBuilder.group({
@@ -20,13 +27,27 @@ export class SubscribeComponent implements OnInit {
       phoneNumber: [null, Validators.pattern(this.phoneNumberRegExp)],
       email: [null, [Validators.required, Validators.email]]
     });
+
+    this.trainId = this.route.snapshot.paramMap.get('id');
   }
 
-  submit(): void {
+  async submit(): Promise<void> {
     if (!this.subscribeForm.valid) {
       return;
     }
-    console.log(this.subscribeForm.value);
+    const signedUps: SignedUp[] = await this.backendService.getSignUpsList();
+    const id = Math.max.apply(Math, signedUps.map(o => o.id));
+
+    this.signedUp = {
+      id: id + 1,
+      name: this.subscribeForm.controls.firstName.value,
+      surname: this.subscribeForm.controls.lastName.value,
+      trainId: this.trainId,
+      email: this.subscribeForm.controls.email.value,
+      phone: this.subscribeForm.controls.phoneNumber.value
+    };
+
+    this.backendService.signUpTraining(this.signedUp).subscribe(evt => console.log('Subscribed!'));
   }
 
 }
--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { SignedUp } from '../models/signedUp';
 import { Training } from '../models/training';
 
@@ -11,11 +12,15 @@ export class BackendService {
   private baseUrl = 'http://localhost:3000';
   constructor(private httpClient: HttpClient) { }
 
-  async getTrainingsList(): Promise<Training[]>{
+  async getTrainingsList(): Promise<Training[]> {
     return await this.httpClient.get<Training[]>(`${this.baseUrl}/trainings`).toPromise();
   }
 
-  async getSignUpsList(): Promise<SignedUp[]>{
-    return await this.httpClient.get<SignedUp[]>(`${this.baseUrl}/signUps`).toPromise();
+  async getSignUpsList(): Promise<SignedUp[]> {
+    return await this.httpClient.get<SignedUp[]>(`${this.baseUrl}/signedUps`).toPromise();
+  }
+
+   signUpTraining(signedUp: SignedUp): Observable<SignedUp>{
+      return this.httpClient.post<SignedUp>(`${this.baseUrl}/signedUps`, signedUp);
   }
 }
